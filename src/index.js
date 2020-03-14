eb7c21f68d074d97fcdf133cbfc154bdac2159c5
Vladislav <superiorite.vladislav@gmail.com>
2020-03-14 18:34:23 +0500

    Create menu without JSX, only JS

--- src/index.js
+++ src/index.js
@@ -1,12 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//Определение Menu
+class Menu extends React.Component {
+	render() {
+		let menus = [
+			'Home',
+			'About',
+			'Services',
+			'Portfolio',
+			'Contact us'
+			]
+		return React.createElement('div',
+			null,
+			menus.map((v, i) => {
+				return React.createElement('div',
+					{key: i},
+					React.createElement(Link, {label: v})
+				)
+			}
+		)
+	)
+}}
+
+
+//Определение комонента Link, который пользуется Menu
+class Link extends React.Component {
+	render() {
+		//Определяет функцию для создания фрагментов
+		//URL из названий команд меню
+		const url='/'
+			+ this.props.label
+			.toLowerCase()
+			.trim()
+			.replace(' ', '-')
+		return React.createElement('div',
+			null,
+			React.createElement(
+				'a',
+				//Передает фрагмент URL для задания атрибута href
+				{href: url},
+				this.props.label
+			),
+			//Добавляет элемент переноса строки для разделения 
+			//элементов меню
+			React.createElement('br')
+		);
+	}
+}
+
+
+
+ReactDOM.render(
+	React.createElement(
+		Menu,
+		null
+	),
+	document.getElementById('root')
+	);
+
